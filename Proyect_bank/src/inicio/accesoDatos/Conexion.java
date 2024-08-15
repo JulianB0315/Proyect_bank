6fcb69c60c2ef3ffac839f72da9a8c0d521cde18
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-14 22:04:31 -0500

    Create Conexion.java

---
+++ Proyect_bank/src/inicio/accesoDatos/Conexion.java
@@ -0,0 +1,19 @@
+package inicio.accesoDatos;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DirverManager:
+/**
+ *
+ * @author Julian
+ */
+public class Conexion {
+    protected Connection conexion=null;
+    
+    public Conexion(){
+        
+    }
+    protected void connectar() throws SQLException, ClassNotFoundException{
+        Class.forName("oracle.jdbc.driver.OracleDirver");
+        conexion = DriverManager.getConnection("jdbc:oracle:thin;@localhost:1521:XE","JULIAN","julian123");
+    }
+}
