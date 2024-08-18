61d7e855d02611ae7852ecadf319c20723a2b522
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-18 14:59:24 -0500

    Update Capaz.java

--- Proyect_bank/src/Controlador/Capaz.java
+++ Proyect_bank/src/Controlador/Capaz.java
@@ -1,9 +1,6 @@
 package Controlador;
 
 import Ventanas.Guia;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Capaz {
     public static void main(String[] args) {
@@ -11,13 +8,4 @@ public class Capaz {
         pantaGuia.setVisible(true);
         pantaGuia.setLocationRelativeTo(null);       
     }
-public class DatabaseConnection {
-    private static final String URL = "jdbc:oracle:thin;@localhost:1521:XE"; 
-    private static final String USER = "JULIAN";
-    private static final String PASSWORD = "julian123"; 
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-}
 }
