fa35287e97b100d65138be1cd2b87bc9a0ec0688
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-27 10:55:07 -0500

    Update controlCapas.java

--- Proyect_bank/src/Controlador/controlCapas.java
+++ Proyect_bank/src/Controlador/controlCapas.java
@@ -2,7 +2,7 @@ package Controlador;
 
 import Ventanas.inicioGuia;
 
-public class Capaz {
+public class controlCapas {
     public static void main(String[] args) {
         inicioGuia pantaGuia = new inicioGuia();
         pantaGuia.setVisible(true);
