c3af49eed04d23d913fa7940869cdb4e764c0e56
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-09-02 13:28:42 -0500

    Create DBConnection.java

---
+++ Proyect_bank_Empleados/src/ventanas/DBConnection.java
@@ -0,0 +1,22 @@
+package ventanas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:oracle:thin:@localhost:1521:XE"; // Cambia el URL de acuerdo a tu configuración
+        String user = "JULIAN";// Usuario
+        String password = "julian123";// Contraseña
+
+        try {
+            Class.forName("oracle.jdbc.OracleDriver"); // Cargar el driver
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Conectado");
+        }
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
